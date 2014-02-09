ef63b6821cb61c56664c97da3dcd20ea7ca6013a
ka <ka.kaosf@gmail.com>
2014-02-09 15:43:02 +0900

    add /hoge and /hoge/fuga routings

--- src/mycompojureapp/handler.clj
+++ src/mycompojureapp/handler.clj
@@ -4,7 +4,9 @@
             [compojure.route :as route]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hello World!!!!")
+  (GET "/hoge" [] "GET /hoge")
+  (GET "/hoge/fuga" [] "GET /hoge/fuga")
   (route/resources "/")
   (route/not-found "Not Found"))
 
